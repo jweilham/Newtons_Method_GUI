63418906ed21c372f0e8edc6969f2a63be6dced4
Jake Weilhammer <40670748+jweilham@users.noreply.github.com>
2019-01-30 15:53:07 -0500

    Update Global.py

--- Global.py
+++ Global.py
@@ -4,4 +4,5 @@ from numpy import linspace
 # x = every number from -10 -> 10  (for graphing functions, graphs function for x values of -10 -> 10)
 # linspace (start, stop, samples)
 # Generate 200 samples to get a smooth line for user's aesthetic pleasure
+
 x_range = linspace(-10, 10, 100)
