4c968e496a5ed79f1e80a42dd89d2e29730bd304
Jake Weilhammer <40670748+jweilham@users.noreply.github.com>
2019-02-01 12:18:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from newton import Newton_GUI
 
-#defines the main function to be run
 def main():
-    gui = Newton_GUI()#creates a new Tk class named app
-    gui.mainloop()#run the app as a mainloop so it keeps running after calculations have been done
+    gui = Newton_GUI()
+    gui.mainloop()
 
-if __name__ == "__main__":#if the name of the class is main, run the main function
+if __name__ == "__main__":
   main()
